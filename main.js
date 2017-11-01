f1a19d4d4b05072435aa962bef07be9bec20789b
MikeStr8s <stratinm@mail.gvsu.edu>
2017-11-01 13:36:08 -0400

    Added the character and squat image logic
    BUG! images returned to document are the avatar images, not the main renders

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function compareCharacterData(charName, charRealm) {
      *************************************************************************************/
 
     if (charName !== "" && charRealm !== "") {    //check if empty strings
-        var URL = "https://us.api.battle.net/wow/character/" + charRealm + "/" + charName + "?locale=en_US&apikey=vhamuv4qktrbzp8hrdamtxkkrps4tv96";
+        var charURL = "https://us.api.battle.net/wow/character/" + charRealm + "/" + charName + "?locale=en_US&apikey=vhamuv4qktrbzp8hrdamtxkkrps4tv96";
         var squatURL = "https://us.api.battle.net/wow/character/wyrmrest%20accord/Squatticus?locale=en_US&apikey=vhamuv4qktrbzp8hrdamtxkkrps4tv96";
         var raceURL = "https://us.api.battle.net/wow/data/character/races?locale=en_US&apikey=vhamuv4qktrbzp8hrdamtxkkrps4tv96";
         var classURL = "https://us.api.battle.net/wow/data/character/classes?locale=en_US&apikey=vhamuv4qktrbzp8hrdamtxkkrps4tv96";
@@ -64,7 +64,7 @@ function compareCharacterData(charName, charRealm) {
         var charData, squatData, classData, raceData;
 
         $.when( //when all of the JSON data is retrieved
-            $.getJSON(URL, function (data) {    //get character data
+            $.getJSON(charURL, function (data) {    //get character data
                 JSON.stringify(data);   //parse character data
                 charData = data;    //save character data
             }),
@@ -86,22 +86,72 @@ function compareCharacterData(charName, charRealm) {
             while (myNode.firstChild) {
                 myNode.removeChild(myNode.firstChild);
             }
+
+            /**
+             * BEGINNING OF THE CHARACTER COMPARISON LOGIC
+             * TODO: Compare charData values with squatData values (as of right now all this does is place charData into HTML document)
+             */
             //place character name into mid
             var charNode = document.createElement('p'); //create paragraph element
             charNode.innerHTML = charData['name'];  //place the name of submitted character into the paragraph element
             myNode.appendChild(charNode);   //add the paragraph element to the mid div
             //place character realm into mid
             var realmNode = document.createElement('p'); //create paragraph element
-            realmNode.innerHTML = charData['realm'];  //place the name of submitted character into the paragraph element
+            realmNode.innerHTML = charData['realm'];  //place the realm of submitted character into the paragraph element
             myNode.appendChild(realmNode);   //add the paragraph element to the mid div
             //place character level into mid
             var levelNode = document.createElement('p'); //create paragraph element
-            levelNode.innerHTML = charData['level'];  //place the name of submitted character into the paragraph element
+            levelNode.innerHTML = charData['level'];  //place the level of submitted character into the paragraph element
             myNode.appendChild(levelNode);   //add the paragraph element to the mid div
             //place character gender into mid
+            var genderNode = document.createElement('p');   //create paragraph element
+            if(charData['gender'] === 0){genderNode.innerHTML = "Male";}    //if gender is male place Male into the paragraph element
+            else{genderNode.innerHTML = "Female";}  //if gender is female place Female into the paragraph element
+            myNode.appendChild(genderNode); //add the paragraph element to the mid div
             //place character race into mid
+            var raceNode = document.createElement('p'); //create paragraph element
+            raceNode.innerHTML = raceData['races'][charData['race'] - 1]['name'];  //place the race of submitted character into the paragraph element
+            myNode.appendChild(raceNode);   //add the paragraph element to the mid div
             //place character class into mid
+            var classNode = document.createElement('p'); //create paragraph element
+            classNode.innerHTML = classData['classes'][charData['class'] - 1]['name'];  //place the class of submitted character into the paragraph element
+            myNode.appendChild(classNode);   //add the paragraph element to the mid div
             //place character faction into mid
+            var factionNode = document.createElement('p');   //create paragraph element
+            if(charData['faction'] === 0){factionNode.innerHTML = "Alliance";}    //if faction is alliance place alliance into the paragraph element
+            else{factionNode.innerHTML = "Horde";}  //if faction is horde place horde into the paragraph element
+            myNode.appendChild(factionNode); //add the paragraph element to the mid div
+            /**
+             * END OF CHARACTER COMPARISON LOGIC
+             */
+
+            /**
+             * BEGINNING OF CHARACTER PICTURE LOGIC
+             */
+            var charJPG, squatJPG;  //these will hold the information to retrieve the static renders from Blizzard
+            charJPG = charData['thumbnail'];
+            squatJPG = squatData['thumbnail'];
+
+            charJPG.replace("avatar", "main");
+            squatJPG.replace("avatar", "main");
+
+            var charImg = document.createElement('img');    //create image element
+            var squatImg = document.createElement('img');   //create image element
+            charImg.setAttribute("src", "https://render-us.worldofwarcraft.com/character/"+charJPG);    //give image source
+            charImg.setAttribute("class", "imgcol");    //give element class
+            squatImg.setAttribute("src", "https://render-us.worldofwarcraft.com/character/"+squatJPG);  //give image source
+            squatImg.setAttribute("class", "imgcol");   //give element class
+
+            var left = document.getElementById("leftside");
+            var right = document.getElementById("rightside");
+            while (left.firstChild) {left.removeChild(left.firstChild);}    //empty left
+            while (right.firstChild) {right.removeChild(right.firstChild);} //empty right
+
+            left.appendChild(squatImg);
+            right.appendChild(charImg);
+            /**
+             * END OF CHARACTER PICTURE LOGIC
+             */
         });
     }
 }
